b9f26de7de1116d726dbf203c750b4e1b4a37fd9
RafaelaCGomes <rafa.carolyne.gomes@gmail.com>
2025-07-07 13:35:07 -0300

    atualização

---
+++ Moodels/Pessoa.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootcamp_WEX.Moodels
+{
+    public class Pessoa
+    {
+//característica da classe, pessoa.
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+//método, ação, função.
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá meu nome é {Nome}, e tenho {Idade} anos.");
+        }
+    }
+}
